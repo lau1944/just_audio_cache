57dda78afa0ad501a555fee3dce149a8f2651905
ToanDK <dokhanhtoan@gapo.com.vn>
2023-10-16 10:56:41 +0700

    fix cache file path missing key

--- lib/src/audio_player_utils.dart
+++ lib/src/audio_player_utils.dart
@@ -92,7 +92,9 @@ extension AudioPlayerExtension on AudioPlayer {
 
   Future<void> cacheFile({required String url, String? path}) async {
     final dirPath = path ?? (await _openDir()).path;
-    final storedPath = await IoClient.download(url: url, path: dirPath);
+    final key = getUrlSuffix(url);
+    final storedPath =
+        await IoClient.download(url: url, path: dirPath + '/' + key);
     if (storedPath != null) {
       _sp!.setString(url, storedPath);
     }
